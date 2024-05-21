e8c49aa0e9bc98e4679950985a5040ceddcab4dc
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2024-05-21 16:00:55 +0900

    Feat: Suspense 이용한 분리
    Promise.all의 경우 속해있는 값들이 전부 loading 이 끝나야 하는데 이를 해결하는 좋은 방안

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,23 +1,9 @@
-import { API_URL } from '../../../(home)/page'
+import { Suspense } from 'react'
+import MovieInfo from '../../../../components/movie-info'
+import MovieVideos from '../../../../components/movie-videos'
 
 export const metadata = {
   title: 'Movies',
-}
-
-async function getMovie(id: string) {
-  console.log(`Fetcing movies: ${Date.now()}`)
-  await new Promise((resolve) => setTimeout(resolve, 3000))
-  const response = await fetch(`${API_URL}/${id}`)
-
-  return response.json()
-}
-
-async function getVideos(id: string) {
-  console.log(`Fetcing videos: ${Date.now()}`)
-  await new Promise((resolve) => setTimeout(resolve, 3000))
-  const response = await fetch(`${API_URL}/${id}/videos`)
-
-  return response.json()
 }
 
 export default async function Movies({
@@ -25,14 +11,14 @@ export default async function Movies({
 }: {
   params: { id: string }
 }) {
-  console.log('start fetching')
-  // 아래처럼 각각 따로 fetch 호출 시, 각 라인이 끝난 후 다음을 호출해 오래걸림.
-  // const movie = await getMovie(id)
-  // const videos = await getVideos(id)
-
-  // 아래처럼 병렬 실행을 해주면 Loading 시간을 기다리지 않고 동시 처리.
-  const [movie, vidoes] = await Promise.all([getMovie(id), getVideos(id)])
-  console.log('end fetching')
-
-  return <h1>{movie.title}</h1>
+  return (
+    <div>
+      <Suspense fallback={<h3>Loading movie info</h3>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h3>Loading movie videos</h3>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  )
 }
