c9e773de1327477995d46c50712795663d0077d4
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2024-05-21 14:38:04 +0900

    Feat: Client Side Data Fetching
    Client Side Date Fetching 경우에는 meta 데이터 사용 불가.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,22 @@
-export const metadata = {
-  title: 'Home',
-}
+'use client'
 
-export default function Tomato() {
-  return <h1>Hello Next JS!</h1>
+import { useEffect, useState } from 'react'
+
+// use client 사용시 metaData 사용 불가
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://nomad-movies.nomadcoders.workers.dev/movies'
+    )
+    const json = await response.json()
+    setMovies(json)
+    setIsLoading(false)
+  }
+  useEffect(() => {
+    getMovies()
+  }, [])
+  return <div>{isLoading ? 'Loading...' : JSON.stringify(movies)}</div>
 }
