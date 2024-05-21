3ccbdc7097155c1b6709f1c9cf67405652448664
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2024-05-21 15:21:02 +0900

    Feat: Loading Component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,8 @@ export const metadata = {
 const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies'
 
 async function getMovies() {
+  // 아래 로딩을 추가했을 때, loading.tsx 없으면 페이지 진입을 못함.
+  await new Promise((resolve) => setTimeout(resolve, 3000))
   const response = await fetch(URL)
   const json = await response.json()
   return json
