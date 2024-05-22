8643011b8304005610b47288ba13c55a29485644
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2024-05-22 09:10:55 +0900

    Feat: Movie Styles

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -3,11 +3,9 @@ import { API_URL } from '../app/(home)/page'
 async function getMovie(id: string) {
   console.log(`Fetcing movies: ${Date.now()}`)
   await new Promise((resolve) => setTimeout(resolve, 3000))
-  // const response = await fetch(`${API_URL}/${id}`)
+  const response = await fetch(`${API_URL}/${id}`)
 
-  // return response.json()
-
-  throw new Error('Not Found')
+  return response.json()
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
