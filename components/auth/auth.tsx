f1b3b544a6f2ebfe4aad9efc5fda1dfcad97886b
Kang-Seonggyu <103415733+Kang-Seonggyu@users.noreply.github.com>
2024-05-29 16:11:17 +0900

    Temp: 포맷 전 백업

--- components/auth/auth.tsx
+++ components/auth/auth.tsx
@@ -20,17 +20,57 @@ export default function AuthBox() {
     }
   }
 
+  async function loginAPI() {
+    const res = await fetch('http://localhost:5174/test/login', {
+      method: 'POST',
+      body: JSON.stringify({ email: form.email, password: form.password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    return res.json()
+  }
+
+  const login = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const res = loginAPI()
+
+    console.log(res)
+  }
+
   return (
     <div id={styles.authbox}>
       <div id={styles.btn} onClick={AuthBoxClick}>
         <Image src="/profile.svg" alt="profile" width={20} height={20} />
         <span>LOG IN</span>
       </div>
-      <div id={styles.myInfo}>
-        <button>LOG OUT</button>
-      </div>
+      {info && (
+        <div id={styles.myInfo}>
+          <button>LOG OUT</button>
+        </div>
+      )}
+
       <Modal open={modal} setOpen={setModal}>
-        <div>모달</div>
+        <form onSubmit={login} id={styles.modalLogin}>
+          <h3>Log In</h3>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) =>
+              setForm((prev) => ({ ...prev, email: e.target.value }))
+            }
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) =>
+              setForm((prev) => ({ ...prev, password: e.target.value }))
+            }
+            required
+          />
+          <button type="submit">Login</button>
+        </form>
       </Modal>
     </div>
   )
